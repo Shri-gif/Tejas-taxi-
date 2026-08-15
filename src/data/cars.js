24e4193eb74c00aff95fedb6b16f13cdcc562981
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-08-15 11:11:07 +0530

    Update cars.js

--- src/data/cars.js
+++ src/data/cars.js
@@ -1,1 +1,49 @@
+export const cars = [
+  {
+    id: "sedan",
+    name: "[SEDAN_MODEL]",
+    image: "/images/sedan.jpg",
+    seats: "4 Passengers",
+    luggage: "2-3 Bags",
+    ac: "AC",
+    oneWay: true,
+    roundTrip: true,
+    price: "Starting from [PRICE]"
+  },
 
+  {
+    id: "suv",
+    name: "[SUV_MODEL]",
+    image: "/images/suv.jpg",
+    seats: "6-7 Passengers",
+    luggage: "4-5 Bags",
+    ac: "AC",
+    oneWay: true,
+    roundTrip: true,
+    price: "Starting from [PRICE]"
+  },
+
+  {
+    id: "ertiga",
+    name: "[ERTIGA_MODEL]",
+    image: "/images/ertiga.jpg",
+    seats: "6-7 Passengers",
+    luggage: "4-5 Bags",
+    ac: "AC",
+    oneWay: true,
+    roundTrip: true,
+    price: "Starting from [PRICE]"
+  },
+
+  {
+    id: "innova",
+    name: "[INNOVA_MODEL]",
+    image: "/images/innova.jpg",
+    seats: "6-7 Passengers",
+    luggage: "5-6 Bags",
+    ac: "AC",
+    oneWay: true,
+    roundTrip: true,
+    price: "Starting from [PRICE]"
+  }
+];
